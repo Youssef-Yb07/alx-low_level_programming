43347df4cae01b26c5c982aa1212bb7f600ba154
Youssef-Yb07 <ybyhb07@gmail.com>
2023-03-27 03:49:42 -0700

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,12 +1,23 @@
-#include "main.h"
-
 /**
- * _atoi - Entry point
- * @s: input
- * Return: Always 0 (Success)
+ * _atoi - convert a string to an integer.
+ * @s: the string to convert.
+ *
+ * Return: the converted integer.
  */
 int _atoi(char *s)
 {
+    int sign = 1, num = 0;
 
-	return (0);
+    while (*s)
+    {
+        if (*s == '-')
+            sign = -sign;
+        else if (*s >= '0' && *s <= '9')
+            num = num * 10 + (*s - '0');
+        else if (num > 0)  /* stop on non-digit after number */
+            break;
+        s++;
+    }
+
+    return sign * num;
 }
