9ed7ff780459870c151bf7b241b0c8e15bb40d01
Youssef-Yb07 <ybyhb07@gmail.com>
2023-04-05 03:50:51 -0700

    0x07. C - Even more pointers, arrays and strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,21 +1,31 @@
 #include "main.h"
 
 /**
- * _strncpy - Entry point
- * @dest: copy to
- * @src: copy from
- * @n: input number of char
- * Return: Always 0 (Success)
+ * _strncpy - copies a string
+ * @dest: initial string
+ * @src: appended string
+ * @n: number of bytes to use from src
+ *
+ * Return: concatinated string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i = 0;
+	int src_size = 0;
 
-	for (i = 0; src[i] != '\0'; i++)
-		if (i < n)
-			dest[i] = src[i];
-		while (i < n)
-			dest[i++] = '\0';
-
+	while (*(src + i))
+	{
+		src_size++;
+		i++;
+	}
+	for (i = 0; i < n; i++)
+	{
+		if (i >= src_size)
+		{
+			*(dest + i) = '\0';
+			continue;
+		}
+		*(dest + i) = *(src + i);
+	}
 	return (dest);
 }
